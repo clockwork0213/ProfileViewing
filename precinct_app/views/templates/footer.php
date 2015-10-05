2593f9768e96ef69d9c723c89d9c10d17189f8b8
Clockwork <clockwork.0509@gmail.com>
2015-10-05 11:11:19 +0800

    Add header.php & footer.php in templates folder

---
+++ precinct_app/views/templates/footer.php
@@ -0,0 +1,6 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+	<script type="text/javascript" src="<?php echo site_url('../assests/js/jquery-1.11.2.js'); ?>"></script>
+	<script type="text/javascript" src="<?php echo site_url('../assests/js/bootstrap.js'); ?>"></script>
+</body>
+</html>
---
+++ precinct_app/views/templates/header.php
@@ -0,0 +1,12 @@
+<?php if(! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Precinct Finder</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="<?php echo site_url("../assests/css/bootstrap.css"); ?>
+</head>
+<body>
+	
